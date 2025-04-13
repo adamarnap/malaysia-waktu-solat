613082b3df26e3dbdbc379559ceaa27e53021f4b
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2025-04-13 09:32:03 +0800

    :zap: add caching headers to API routes
    https://laravel.com/docs/12.x/responses#cache-control-middleware

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,16 @@ use App\Http\Controllers\api\v1\ZonesController;
 use App\Http\Controllers\api\v2\PrayerTimeContoller;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/v2/solat/{zone}', [PrayerTimeContoller::class, 'fetchMonth'])->name('v2.solat.month');
-Route::get('/v2/solat/{lat}/{long}', [PrayerTimeContoller::class, 'fetchMonthLocationByGps'])->name('v2.solat.month_with_gps');
+Route::middleware('cache.headers:public;max_age=3600;etag')->group(function () {
+    Route::get('/v2/solat/{zone}', [PrayerTimeContoller::class, 'fetchMonth'])->name('v2.solat.month');
+    Route::get('/v2/solat/{lat}/{long}', [PrayerTimeContoller::class, 'fetchMonthLocationByGps'])->name('v2.solat.month_with_gps');
 
-Route::get('/solat/{zone}', [PrayerTimeV1Contoller::class, 'fetchMonth'])->name('v1.solat.month');
-Route::get('/solat/{zone}/{day}', [PrayerTimeV1Contoller::class, 'fetchDay'])->name('v1.solat.day');
+    Route::get('/solat/{zone}', [PrayerTimeV1Contoller::class, 'fetchMonth'])->name('v1.solat.month');
+    Route::get('/solat/{zone}/{day}', [PrayerTimeV1Contoller::class, 'fetchDay'])->name('v1.solat.day');
 
-Route::prefix('zones')->group(function () {
-    Route::get('/', [ZonesController::class, 'index'])->name('zones.index');
-    Route::get('/{state}', [ZonesController::class, 'getByState'])->name('zones.state');
-    Route::get('/{lat}/{long}', [ZonesController::class, 'getZoneFromCoordinate'])->name('zones.gps');
+    Route::prefix('zones')->group(function () {
+        Route::get('/', [ZonesController::class, 'index'])->name('zones.index');
+        Route::get('/{state}', [ZonesController::class, 'getByState'])->name('zones.state');
+        Route::get('/{lat}/{long}', [ZonesController::class, 'getZoneFromCoordinate'])->name('zones.gps');
+    });
 });
