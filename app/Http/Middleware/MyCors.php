f6d59d2510a77ed5a8513a0d66faf3ef95921f13
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2025-06-17 05:30:23 +0800

    ✨ add Access-Control-Allow-Origin to all header

---
+++ app/Http/Middleware/MyCors.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class MyCors
+{
+    /**
+     * Add Access-Control headers to the response.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        // Add CORS headers for GET requests
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type');
+
+        return $response;
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
 use App\Http\Middleware\LogApiRequests;
+use App\Http\Middleware\MyCors;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -27,6 +28,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->api(append: [
+            MyCors::class,
             LogApiRequests::class,
         ]);
     })
